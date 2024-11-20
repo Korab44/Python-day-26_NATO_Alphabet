5242a85292a57f404ca346789f18ab597669973c
Korab <korab.morina44@gmail.com>
2024-11-20 19:12:58 +0200

    exception handling

--- main.py
+++ main.py
@@ -8,6 +8,14 @@ dict_format = {row.letter: row.code for (index, row) in data_nato.iterrows()}
 
 
 
-user_input = input("Choose a word? ").upper()
-list_user_input = [value for (letter, value) in dict_format.items()if letter in user_input]
-print(list_user_input)
+def generate_phonetic():
+    user_input = input("Choose a word? ").upper()
+    try:
+        list_user_input = [dict_format[letter] for letter in user_input]
+    except KeyError:
+        print("Sorry, only letters please")
+        generate_phonetic()
+    else:
+        print(list_user_input)
+
+generate_phonetic()
